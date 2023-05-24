b65efccaec163225f738ac205737cc5ce7eb0b8b
Unyeong Kang <dnsdud426@naver.com>
2023-05-24 13:06:46 +0900

    projectBulid

---
+++ src/main/java/com/example/tobyspring3/Tobyspring3Application.java
@@ -0,0 +1,13 @@
+package com.example.tobyspring3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tobyspring3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tobyspring3Application.class, args);
+	}
+
+}
